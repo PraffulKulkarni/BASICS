ab38cce525332739a456d93043c38b3562fdd6bb
Prafful Kulkarni <160165503+PraffulKulkarni@users.noreply.github.com>
2024-07-31 22:34:16 +0530

    Experimnt_3.cpp

---
+++ Experimnt_3.cpp
@@ -0,0 +1,71 @@
+// Prafful Kulkarni
+//23070123099
+//entc b1
+//Experiment 3-To study and implement Operators in C++
+
+//exp 3.1
+
+//arithmetic operator: 
+
+ #include<iostream>
+ using namespace std;
+ int main()
+{
+    int a,b;
+    cout<<"enter first number: ";
+    cin>>a;
+    cout<<"enter second number: ";
+    cin>>b;
+    cout<<"a==b: "<<(a==b)<<endl;
+    return 0;
+}
+
+//Relational  oprator:
+
+ #include<iostream>
+ using namespace std;
+ int main()
+{
+    int a,b;
+    cout<<"enter first number: ";
+    cin>>a;
+    cout<<"enter second number: ";
+    cin>>b;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    return 0;
+}
+
+//exp 3.2 logical operator:
+
+ #include<iostream>
+ using namespace std;
+ int main()
+{
+    
+    
+    bool a = true;
+    
+    bool b = true;
+    cout<<boolalpha;
+    cout<<"a&&b: "<<(a&&b)<<endl;
+    return 0;
+}
+
+//exp 3.3:
+
+ #include<iostream>
+ using namespace std;
+ int main()
+{ int a,b;
+    
+    a = 1;
+    b = 1;
+    cout<<"a&b: "<<(a&b)<<endl;
+     cout<<"a|b: "<<(a|b)<<endl;
+      cout<<"a^b: "<<(a^b)<<endl;
+       cout<<"a: "<<(~a)<<endl;
+        cout<<"a<<b: "<<(a<<b)<<endl;
+         cout<<"a>>b: "<<(a>>b)<<endl;
+    
+    return 0;
+}
