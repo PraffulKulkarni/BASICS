c072bce6fd748554f6137fd0c96717ead28d466e
Prafful Kulkarni <160165503+PraffulKulkarni@users.noreply.github.com>
2024-08-01 15:04:35 +0530

    Experiment 6.cpp

---
+++ Experiment 6.cpp
@@ -0,0 +1,13 @@
+// Prafful Kulkarni
+//23070123099
+//experiment 6
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    int i;
+    for(i=0;i<10;i++){
+        cout<<"Hello world("<<i+1<<")"<<endl;
+    }
+}
