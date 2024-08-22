ea26acea13e674abb9404423128a979bebd334c6
Prafful Kulkarni <160165503+PraffulKulkarni@users.noreply.github.com>
2024-08-22 16:19:26 +0530

    experiment_8.cpp

--- experiment_8.cpp
+++ experiment_8.cpp
@@ -44,4 +44,80 @@ Enter element(1,1)4
 34
 
   //addition of matrices:
+
+     #include<iostream>
+using namespace std;
+int main()
+{
+     int i,j,row,col;
+   cout<<"enter row and column: ";
+   cin>>row>>col;
+   
+   int arr1[row][col];
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            cout<<"Enter element("<<i<<","<<j<<")";
+            cin>>arr1[i][j];
+             
+        }
+        
+    }
+    cout<<"enter row and column: ";
+   cin>>row>>col;
+    int arr2[row][col];
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            cout<<"Enter element("<<i<<","<<j<<")";
+            cin>>arr2[i][j];
+             
+        }
+        
+    }
+    
+    int sum[row][col];
+     for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+          sum[i][j]=arr1[i][j]+arr2[i][j];
+             
+        }
+        
+    }
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+        {
+            cout<<sum[i][j];
+             
+        }
+        cout<<endl;
+        
+    }  
+    
+    
+      
+   
+    
+    
+}
+
+//output:
+enter row and column: 2 2
+Enter element(0,0)1 
+Enter element(0,1)2
+Enter element(1,0)3
+Enter element(1,1)4
+enter row and column: 2 2
+Enter element(0,0)2
+Enter element(0,1)3
+Enter element(1,0)4
+Enter element(1,1)5
+35
+79
+
   
