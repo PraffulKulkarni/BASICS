5d82ca86df70bd155e6c737317a55eba907866b5
Prafful Kulkarni <160165503+PraffulKulkarni@users.noreply.github.com>
2024-08-23 10:20:32 +0530

    experiment_8.cpp

--- experiment_8.cpp
+++ experiment_8.cpp
@@ -121,4 +121,87 @@ Enter element(1,1)5
 79
 
 // multiplication of arrays:
+     #include <iostream>  
+using namespace std;  
+int main()  
+{  
+int r,c,i,j,k,r1,c1;    
+cout<<"enter the number of row=";    
+cin>>r;    
+cout<<"enter the number of column=";    
+cin>>c;    
+int a[r][c];
+   
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{  
+    cout<<"Enter element of first matrix("<<i<<","<<j<<")"; 
+cin>>a[i][j];  
+}    
+}   
+cout<<"enter the number of row=";    
+cin>>r1;    
+cout<<"enter the number of column=";    
+cin>>c1;  
+int b[r1][c1], mul[r][c1];
+   
+for(i=0;i<r1;i++)    
+{    
+for(j=0;j<c1;j++)    
+{  
+    cout<<"Enter element ofsecond matrix("<<i<<","<<j<<")";
+cin>>b[i][j];    
+}    
+}  
+if(r1==c)
+{
+cout<<"multiplication of the matrix=\n";    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c1;j++)    
+{    
+mul[i][j]=0;    
+for(k=0;k<c;k++)    
+{    
+mul[i][j]+=a[i][k]*b[k][j];    
+}    
+}    
+}    
      
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c1;j++)    
+{    
+cout<<mul[i][j]<<" ";    
+}    
+cout<<"\n";    
+}   
+}
+else
+{
+    cout<<"error";
+}
+return 0;  
+}    
+//output:
+enter the number of row=2
+enter the number of column=3
+Enter element of first matrix(0,0)12
+Enter element of first matrix(0,1)33
+Enter element of first matrix(0,2)4
+Enter element of first matrix(1,0)5
+Enter element of first matrix(1,1)6
+Enter element of first matrix(1,2)4
+enter the number of row=3
+enter the number of column=2
+Enter element ofsecond matrix(0,0)1
+Enter element ofsecond matrix(0,1)2
+Enter element ofsecond matrix(1,0)4
+Enter element ofsecond matrix(1,1)6
+Enter element ofsecond matrix(2,0)7
+Enter element ofsecond matrix(2,1)6
+multiplication of the matrix=
+161 224 
+57 70 
+
